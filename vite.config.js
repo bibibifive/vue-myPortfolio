f1d9ef11ccb68cec45cddb919bc0a872646c3226
molid <2867293046@qq.com>
2023-02-08 14:55:30 +0800

    build config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base:"./",
+  build:{
+    outDir:"../vue-myPortfolio-build"
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
