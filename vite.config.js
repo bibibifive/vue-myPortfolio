e65f42732c3892345db5038cb4ca5cadd0814437
molid <2867293046@qq.com>
2023-05-16 21:57:47 +0800

    docs

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import viteCompression from 'vite-plugin-compression';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          // 标签中以 -icon 结尾的均视为自定义元素
-          isCustomElement: (tag) => tag.endsWith('-icon'),
-        },
-      },
-    }),
-    viteCompression(),
-  ],
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        // 标签中以 -icon 结尾的均视为自定义元素
+        isCustomElement: (tag) => tag.endsWith('-icon')
+      }
+    }
+  })],
   base: './',
   build: {
     outDir: './docs',
